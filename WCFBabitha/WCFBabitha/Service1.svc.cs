e25717f608c91116cad15a494d624f21808b8a1b
babithareddy <bbf47@mail.umkc.edu>
2015-02-27 12:07:03 -0600

    Assignment

---
+++ WCFBabitha/WCFBabitha/Service1.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFBabitha
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetLoginData/login/{id}")]
+        public logindetails GetLoginData(String id)
+        {
+
+            return new logindetails()
+            {
+               
+            password = "xxxx",
+             userid = id
+
+            };
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+
+    public class logindetails
+        {
+  public  String userid;
+   public  String password;
+
+}
+  
+}
